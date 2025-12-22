928cd706f91f51e2dfd9add91c67559705e3eab2
Andisheh81 <applezangene@gmail.com>
2025-12-22 15:45:16 +0300

    i did my second practice too

--- main.py
+++ main.py
@@ -14,3 +14,18 @@ print(f"Full Name: [{name}]")
 print(f"Age: [{age}]")
 print(f"Secret Username: [{secretname}]")
 print(f"Height: [{height}]m")
+
+if age>18:
+    print("Access Granted: You are an adult.")
+else:
+    print("Access Restricted: Minor detected.")
+
+if height>2.00:
+    print("You are very tall!")
+elif height<2.00 and height>1.50:
+    print("You are average height.")
+elif height<1.50:
+    print("You are a bit short!")
+if fname=="":
+    fname="guest"
+
